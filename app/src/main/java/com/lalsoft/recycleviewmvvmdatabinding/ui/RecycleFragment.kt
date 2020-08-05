88a2bc6593778db518d9e193ea5ddf27a7a2a9ba
Shijilal <shijilal@hotmail.com>
2020-08-05 09:49:48 +0530

    Working with RecyclerView code in Fragment

--- app/src/main/java/com/lalsoft/recycleviewmvvmdatabinding/ui/RecycleFragment.kt
+++ app/src/main/java/com/lalsoft/recycleviewmvvmdatabinding/ui/RecycleFragment.kt
@@ -1,7 +1,47 @@
 package com.lalsoft.recycleviewmvvmdatabinding.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.lalsoft.recycleviewmvvmdatabinding.R
+import com.lalsoft.recycleviewmvvmdatabinding.adapter.RecycleAdapter
+import com.lalsoft.recycleviewmvvmdatabinding.databinding.RecycleFragmentBinding
+import com.lalsoft.recycleviewmvvmdatabinding.model.User
+import com.lalsoft.recycleviewmvvmdatabinding.viewmodel.RecycleViewModel
 
-class RecycleFragment:Fragment() {
+class RecycleFragment : Fragment() {
+    lateinit var dataBinding: RecycleFragmentBinding
+    lateinit var viewModel:RecycleViewModel
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.recycle_fragment, container, false)
+        return dataBinding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel=ViewModelProvider(this).get(RecycleViewModel::class.java)
+        viewModel.userList.observe(viewLifecycleOwner,userListObserver)
+
+
+
+    }
+    private val userListObserver=Observer<ArrayList<*>>{
+        val recyclerView =dataBinding.recyclerViewRV
+        recyclerView.layoutManager = GridLayoutManager(context,2,LinearLayoutManager.VERTICAL,false)
+        //recyclerView.layoutManager = LinearLayoutManager(context)
+        val adapter=RecycleAdapter()
+        adapter.setUserList(it)
+        recyclerView.adapter = adapter
+    }
 }
--- app/src/main/java/com/lalsoft/recycleviewmvvmdatabinding/viewmodel/RecycleViewModel.kt
+++ app/src/main/java/com/lalsoft/recycleviewmvvmdatabinding/viewmodel/RecycleViewModel.kt
@@ -1,17 +1,28 @@
 package com.lalsoft.recycleviewmvvmdatabinding.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.lalsoft.recycleviewmvvmdatabinding.adapter.RecycleAdapter
 import com.lalsoft.recycleviewmvvmdatabinding.data.Repository
+import com.lalsoft.recycleviewmvvmdatabinding.model.User
 
-class RecycleViewModel :ViewModel(){
+class RecycleViewModel : ViewModel() {
     private val repository by lazy { Repository() }
-    var adapter:RecycleAdapter?=null
-    private set
+
+    //    var adapter:RecycleAdapter?=null
+//    private set
+    private val _adapter: MutableLiveData<RecycleAdapter> = MutableLiveData()
+    val adapter: LiveData<RecycleAdapter>
+        get() = _adapter
+    private val _userList: MutableLiveData<ArrayList<*>> = MutableLiveData()
+    val userList: LiveData<ArrayList<*>>
+        get() = _userList
+
 
     init {
-        val userList=repository.getUserList()
-        adapter?.setUserList(userList)
+        _userList.value = repository.getUserList()
+        //_adapter.value?.setUserList(userList)
     }
 
 }
